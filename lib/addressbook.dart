78ada67ad0c63bfd1e37c12a7fce8b284bd520f9
Jagmit Singh <js@skalio.com>
2020-10-13 16:29:34 +0200

    Adds documentation for library methods/fields/classes

--- lib/addressbook.dart
+++ lib/addressbook.dart
@@ -2,11 +2,19 @@ import 'dart:async';
 
 import 'package:flutter/services.dart';
 
+/// Class on which addressbook operations can be called via static methods.
 class Addressbook {
   static const MethodChannel _channel = const MethodChannel('addressbook');
 
-  static Future<List<Contact>> getContacts(
-      {String query, bool onlyWithEmail, bool profileImage}) async {
+  /// Returns a [Future] that completes with a [List] of all available [Contacts].
+  /// The search space can be reduces by passing a [query] string to which the results must match.
+  /// Setting [onlyWithEmail] will filter out contacts without a single [Contacts#emailAddresses] entry.
+  /// Profile image data can be included with the [profileImage] flag.
+  static Future<List<Contact>> getContacts({
+    String query,
+    bool onlyWithEmail,
+    bool profileImage,
+  }) async {
     List<dynamic> contacts = await _channel.invokeMethod(
       'getContacts',
       {
@@ -59,12 +67,26 @@ class Addressbook {
   }
 }
 
+/// Entity representing one contact in an addressbook.
 class Contact {
+  /// Given name alias forename.
   final String givenName;
+
+  /// Family name alias surname.
   final String familyName;
+
+  /// The organization this contact belongs to.
   final String organization;
+
+  /// A collection of all the emailAddresses attached to this contact.
+  /// The key represents the type of the emailAddress (work, home, etc.) and the value is the actually emailAddress.
   final Map<String, String> emailAddresses;
+
+  /// A collection of all the phoneNumbers attached to this contact.
+  /// The key represents the type of the phoneNumber (home, mobile, etc.) and the value is the actually phoneNumber as a [String].
   final Map<String, String> phoneNumbers;
+
+  /// Base64-encoded profile picture
   final String profileImage;
 
   Contact(this.givenName, this.familyName, this.organization,
